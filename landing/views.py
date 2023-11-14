963c4564aedf23a4e3bac8cbe0aad70bf6d9b523
sajidifti <info@sajidifti.com>
2023-11-15 04:48:54 +0600

    initial

---
+++ landing/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
+from django.conf import settings
+import os
+# Import mimetypes module
+import mimetypes
+# import os module
+import os
+# Import HttpResponse module
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def CV(request):
+    return render(request, 'CV.html')
+
+
+def WLASL(request):
+    return render(request, 'WLSLT.html')
+
+
+def download_file(request, filename):
+    context = {'filename': filename}
+
+    if filename != '':
+        # Define Django project base directory
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        # Define the full file path
+        filepath = BASE_DIR + '/files/' + filename
+        # Open the file for reading content
+        path = open(filepath, 'rb')
+        # Set the mime type
+        mime_type, _ = mimetypes.guess_type(filepath)
+        # Set the return value of the HttpResponse
+        response = HttpResponse(path, content_type=mime_type)
+        # Set the HTTP header for sending to browser
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        # Return the response value
+        return response
+    else:
+        # Load the template
+        return render(request, 'download.html', context)
---
+++ portfolio/urls.py
@@ -0,0 +1,40 @@
+"""portfolio URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from landing.views import home, download_file, WLASL, CV
+from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('WLASL/', WLASL, name='WLASL'),
+    path('CV/', CV, name='CV'),
+    path('download/<str:filename>/', download_file, name='download_file'),
+    # path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
