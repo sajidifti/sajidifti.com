9d16b0719e8bde0796e6639cc262f8588649451a
sajidifti <info@sajidifti.com>
2024-02-25 01:50:44 +0600

    Modified admin.py for contacts

--- landing/admin.py
+++ landing/admin.py
@@ -3,5 +3,13 @@ from .models import Project, Contact
 
 # Register your models here.
 
+
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message', 'created_at')
+    readonly_fields = ('created_at',)
+
+
 admin.site.register(Project)
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
