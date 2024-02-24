add475005fa292131af8b73e981d2e6a6a5081f7
sajidifti <info@sajidifti.com>
2024-02-25 02:10:20 +0600

    custom admin for project and contact

--- landing/admin.py
+++ landing/admin.py
@@ -4,12 +4,15 @@ from .models import Project, Contact
 # Register your models here.
 
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('project_name', 'priority')
+    ordering = ('-priority',)
 
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'message', 'created_at')
+    list_display = ('name', 'created_at')
     readonly_fields = ('created_at',)
 
 
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Contact, ContactAdmin)
